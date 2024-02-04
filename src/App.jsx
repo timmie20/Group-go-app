8b01fb6a447fc1740f4392868a376e9eeb637c09
Timilehin <olutimilehinolayinka1234@gmail.com>
2024-02-04 22:09:37 +0100

    Built signin UI, added firebase auth and env file

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import AppLayout from "./layouts/AppLayout";
 import { AppProvider } from "./context/AppContext";
 import Home from "./Pages/Home";
 import CreateEvent from "./Pages/CreateEvent";
+import Signin from "./components/Signin";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <>
-      <AppProvider>
-        <AppLayout>
-          <Routes>
-            <Route path="/">
-              <Route index element={<Home />}/>
-              <Route path="/create" element={<CreateEvent />} />
-            </Route>
-          </Routes>
-        </AppLayout>
-      </AppProvider>
+      <AuthContextProvider>
+        <AppProvider>
+          <AppLayout>
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="/create" element={<CreateEvent />} />
+              </Route>
+              <Route path="/signin" element={<Signin />} />
+            </Routes>
+          </AppLayout>
+        </AppProvider>
+      </AuthContextProvider>
     </>
   );
 };
---
+++ src/components/Signin.jsx
@@ -0,0 +1,84 @@
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { FcGoogle } from "react-icons/fc";
+import { AnimatePresence, motion } from "framer-motion";
+import { HiOutlineBellAlert } from "react-icons/hi2";
+
+const Signin = () => {
+  const {
+    email,
+    setEmail,
+    sendEmailLink,
+    handleSignInUser,
+    navigate,
+    user,
+    search,
+    alertMsg,
+  } = useContext(AuthContext);
+
+  useEffect(() => {
+    handleSignInUser();
+  }, [user, navigate, search]);
+
+  return (
+    <>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          onClick={() => setIsOpen(false)}
+          className="fixed inset-0 z-50 grid cursor-pointer place-items-center overflow-y-scroll bg-slate-900/20 p-8 backdrop-blur"
+        >
+          <motion.div
+            initial={{ scale: 0, rotate: "12.5deg" }}
+            animate={{ scale: 1, rotate: "0deg" }}
+            exit={{ scale: 0, rotate: "0deg" }}
+            onClick={(e) => e.stopPropagation()}
+            className="relative w-full max-w-lg cursor-default overflow-hidden rounded-lg bg-white px-14 py-16 shadow-xl"
+          >
+            <form onSubmit={(event) => sendEmailLink(event, email)}>
+              <div>
+                <h2>groupgo</h2>
+                <p>Sign in or sign up to create an event</p>
+              </div>
+              <div className="mt-6 flex flex-col gap-[22px]">
+                <fieldset className="field_set_div">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    className="inputs"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  {alertMsg && (
+                    <div className="flex items-center gap-1">
+                      <HiOutlineBellAlert color="red" />
+                      <span className="text-[12px]">{alertMsg}</span>
+                    </div>
+                  )}
+                </fieldset>
+                <div>
+                  <button
+                    className="signin_btn bg-orange-clr text-white "
+                    type="submit"
+                  >
+                    continue with email
+                  </button>
+                  <div className="signin_btn mt-3 flex items-center justify-center gap-2 bg-[#F8F8F8] text-black-clr">
+                    <FcGoogle size={24} />
+                    <span>Continue with google</span>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </motion.div>
+        </motion.div>
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default Signin;
