d7c89d5085304eddcee91854556c4c37ed681cac
Jeffrey Uzoamaka <ikechukwujeffrey19@gmail.com>
2024-02-14 10:20:28 +0100

    Added userdoc for google auth & added profile image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import Avatar from "../assets/images/avatar.png"
 
 const Navbar = () => {
-  const { handleLogOut } = useContext(AuthContext);
+  const { handleLogOut, user } = useContext(AuthContext);
+  const [toggleDropDown, setToggleDropDown] = useState(false)
+
+  useEffect(() => {
+    console.log(user)
+  }, [user])
   return (
     <>
       <nav className="">
         <div className="nav_items">
           <h4>groupgo</h4>
-          <ul>
+          <ul className="relative">
             <li>Home</li>
             <li>Features</li>
             <li>How it works</li>
             <li>About us</li>
-            <button className="rounded-[10px] bg-orange-clr px-[18px] py-2 text-white">
+            {!user && <button className="rounded-[10px] bg-orange-clr px-[18px] py-2 text-white">
               Create event
-            </button>
-            <button onClick={handleLogOut}>logout</button>
+            </button>}
+            {user && <img onClick={() => setToggleDropDown(prev => !prev)} className="w-[48px] h-[48px] rounded-[999px] cursor-pointer" src={`${user.photoURL || Avatar}`} alt="" />}
+            
+
+            {user && toggleDropDown && <div className="shadow-lg flex flex-col items-start gap-[16px] rounded-[16px] px-[18px] py-[18px] absolute z-50 bg-white right-0 top-[50px]">
+              <p className="text-[16px]">{user?.email}</p>
+              <button onClick={handleLogOut}>logout</button>
+            </div>}
           </ul>
         </div>
       </nav>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_API_KEY,
@@ -15,4 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app)
 const analytics = getAnalytics(app);
